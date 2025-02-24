b3ec535456b79e00c8bf75b6b4d79be0eb241186
njuhasz <juhasznorbi@gmail.com>
2025-02-24 17:14:36 +0100

    add vitest and eslint

--- test-assignments/frontend-react-assignment/vite.config.ts
+++ test-assignments/frontend-react-assignment/vite.config.ts
@@ -1,8 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/test/setup.ts'
+  }
 })
