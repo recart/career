9070802c7691797ebb9d8448afb648812248c959
njuhasz <juhasznorbi@gmail.com>
2025-02-24 16:22:08 +0100

    replace create-react-app with vite

--- test-assignments/frontend-react-assignment/src/components/colleague-row/colleague-row.component.tsx
+++ test-assignments/frontend-react-assignment/src/components/colleague-row/colleague-row.component.tsx
@@ -1,6 +1,6 @@
-import { ReactComponent as StarFillIcon } from '../../assets/star-fill.svg'
-import { ReactComponent as StarIcon } from '../../assets/star.svg'
-import { ReactComponent as TrashIcon } from '../../assets/trash.svg'
+import StarFillIcon from '../../assets/star-fill.svg'
+import StarIcon from '../../assets/star.svg'
+import TrashIcon  from '../../assets/trash.svg'
 import { useAppDispatch } from '../../hooks/redux'
 import { favoriteColleague, removeColleague } from '../../store/reducers/colleagues.reducer'
 import { Colleague } from '../../store/types/colleague.type'
@@ -31,14 +31,14 @@ export function ColleagueRow(props: ColleagueProps) {
           className='bg-gray-200 p-2 border border-gray-400 rounded hover:bg-gray-100'
           onClick={handleFavorite}
         >
-          {colleague.favorite ? <StarFillIcon /> : <StarIcon />}
+          {colleague.favorite ? <img src={StarFillIcon} alt='favorited' /> : <img src={StarIcon} alt='favorite' />}
         </button>
         <button
           type='button'
           className='bg-gray-200 p-2 border border-gray-400 rounded hover:bg-gray-100'
           onClick={handleDelete}
         >
-          <TrashIcon />
+          <img src={TrashIcon} alt='delete' />
         </button>
       </div>
     </div>
